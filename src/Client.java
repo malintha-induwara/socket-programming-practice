97b9d8a6536727f31e7c28c83a2aee0a95a91e44
Induwara <induwara2k@gmail.com>
2024-01-06 13:06:12 +0530

    basic feauteres implemented

--- src/Client.java
+++ src/Client.java
@@ -1,29 +1,34 @@
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
-
-
-
         try {
-            Socket socket = new Socket("localhost",2000);
+            Socket socket = new Socket("localhost", 2000);
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-            dataOutputStream.writeUTF("Hello Server");
-            dataOutputStream.flush();
+            String reply;
+            String message;
 
+            do {
+                reply=bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
 
+                message = dataInputStream.readUTF();
+                System.out.println("Server: " + message);
+            }while (!message.equals("end"));
 
-            dataOutputStream.close();
             socket.close();
+            dataOutputStream.close();
+            dataInputStream.close();
+            bufferedReader.close();
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
     }
 }
-
--- src/Server.java
+++ src/Server.java
@@ -1,32 +1,47 @@
-import java.io.DataInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
-        try {
 
-            //Creating Server socket
+        try {
+            // Creating Server socket
             ServerSocket serverSocket = new ServerSocket(2000);
             System.out.println("Server started");
 
-            //Accept the connection and Creating Local Socket
+            // Accept the connection and Creating Local Socket
             Socket localSocket = serverSocket.accept();
-
             System.out.println("Client Request Accepted!");
-
-
 
             DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
 
-            //Reading the massage
-            String massage = dataInputStream.readUTF();
-            System.out.println("Client: "+massage);
+            DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
 
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+            String reply;
+            String message;
+
+            do {
+                reply=bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+
+                message = dataInputStream.readUTF();
+                System.out.println("Client: " + message);
+            }while (!message.equals("end"));
+
+            localSocket.close();
+            dataOutputStream.close();
+            dataInputStream.close();
+            bufferedReader.close();
+
+        }catch (IOException e){
+
         }
     }
 }
-
